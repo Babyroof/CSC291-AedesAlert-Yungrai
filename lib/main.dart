3672eb5b8d69bd5adc1023371388358efbd50967
natebps <natebpss@gmail.com>
2026-05-14 23:19:36 +0700

    feat: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'seed_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await seedAll(); // Call the seedData function
   runApp(const MyApp());
 }
 
